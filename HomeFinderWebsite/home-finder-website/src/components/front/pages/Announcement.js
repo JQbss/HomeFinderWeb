cb21980a93b7f30438dea0f3359075950415d0a7
Vsevolod Panasov <4erniikot@gmail.com>
2022-04-10 23:00:01 +0200

    added announcement page

---
+++ HomeFinderWebsite/home-finder-website/src/components/front/pages/Announcement.js
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { Oval } from "react-loader-spinner";
+import FetchManager from "../../../classes/FetchManager";
+
+const Announcement = (props) => {
+  const { announcementId } = useParams();
+  const [annData, setAnnData] = useState();
+
+  useEffect(() => {
+    FetchManager.GetOne("announcement", announcementId).then((data) =>
+      setAnnData(data)
+    );
+  }, []);
+
+  return annData ? (
+    <>
+      <div className="ann-details-root">
+        <div className="ann-details-container">
+          <h2>{annData?.title}</h2>
+
+          <div className="ann-details-body">
+            <div>
+              <img src={"/house-logo-1.png"} width={300} style={{marginRight:30}} />
+            </div>
+            <div className="ann-details-descr">
+              {annData?.link && (
+                <a href={`${annData.link}`} target="_blank">
+                  <h3>Link do ogłoszenia </h3>
+                </a>
+              )}
+              <h3>Opis:</h3>
+              <br />
+              {annData?.description}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  ) : (
+    <Oval color="#00BFFF" height={80} width={80} />
+  );
+};
+
+export default Announcement;
