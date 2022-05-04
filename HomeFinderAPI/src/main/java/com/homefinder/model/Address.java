074b9fcf9405cd9087311a63869c43026d9fb226
Olena Savelieva <savelieva372@gmail.com>
2022-05-04 21:53:16 +0200

    Add constructor and getter and setter in address

--- HomeFinderAPI/src/main/java/com/homefinder/model/Address.java
+++ HomeFinderAPI/src/main/java/com/homefinder/model/Address.java
@@ -9,4 +9,70 @@ public class Address {
     private String ulica;
     private String numer_domu;
     private String numer_mieszkania;
+
+    Address(){ }
+
+    public String getWojewodztwo() {
+        return wojewodztwo;
+    }
+
+    public void setWojewodztwo(String wojewodztwo) {
+        this.wojewodztwo = wojewodztwo;
+    }
+
+    public String getPowiat() {
+        return powiat;
+    }
+
+    public void setPowiat(String powiat) {
+        this.powiat = powiat;
+    }
+
+    public String getMiejscowosc() {
+        return miejscowosc;
+    }
+
+    public void setMiejscowosc(String miejscowosc) {
+        this.miejscowosc = miejscowosc;
+    }
+
+    public String getGmina() {
+        return gmina;
+    }
+
+    public void setGmina(String gmina) {
+        this.gmina = gmina;
+    }
+
+    public String getKod_pocztowy() {
+        return kod_pocztowy;
+    }
+
+    public void setKod_pocztowy(String kod_pocztowy) {
+        this.kod_pocztowy = kod_pocztowy;
+    }
+
+    public String getUlica() {
+        return ulica;
+    }
+
+    public void setUlica(String ulica) {
+        this.ulica = ulica;
+    }
+
+    public String getNumer_domu() {
+        return numer_domu;
+    }
+
+    public void setNumer_domu(String numer_domu) {
+        this.numer_domu = numer_domu;
+    }
+
+    public String getNumer_mieszkania() {
+        return numer_mieszkania;
+    }
+
+    public void setNumer_mieszkania(String numer_mieszkania) {
+        this.numer_mieszkania = numer_mieszkania;
+    }
 }
