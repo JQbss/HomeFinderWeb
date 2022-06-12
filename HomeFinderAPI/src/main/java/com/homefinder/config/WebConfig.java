5d4e1a338f8a2eecd92538cb84831232b40e6c95
Olena Savelieva <savelieva372@gmail.com>
2022-06-12 20:20:20 +0200

    cors

--- HomeFinderAPI/src/main/java/com/homefinder/config/WebConfig.java
+++ HomeFinderAPI/src/main/java/com/homefinder/config/WebConfig.java
@@ -13,6 +13,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:8080","http://localhost:3000");
+        registry.addMapping("/**").allowedOrigins("http://localhost:8080/**","http://localhost:3000/**")
+                .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+
     }
+
 }
